7fc42189ce518a36fcb876cff6cf401e592cd283
Simon Soriano <simon0191@gmail.com>
2016-11-28 22:58:40 -0500

    Setup email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.default_url_options = { host: ENV['SS_HOST'] }
+
+  config.action_mailer.smtp_settings = {
+      address:                ENV['EMAIL_SMTP_ADDRESS'],
+      port:                   ENV['EMAIL_SMTP_PORT'],
+      domain:                 ENV['SS_HOST'],
+      user_name:              ENV['SENDGRID_USERNAME'],
+      password:               ENV['SENDGRID_PASSWORD'],
+      authentication:         ENV['EMAIL_AUTHENTICATION'],
+      enable_starttls_auto: !!ENV['EMAIL_ENABLE_STARTTLS_AUTO']
+  }
 end
