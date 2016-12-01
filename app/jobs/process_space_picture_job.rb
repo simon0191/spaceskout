1105cf1e3b7fc8d4875b1765da02fd4797829494
Simon Soriano <simon0191@gmail.com>
2016-11-30 19:23:19 -0500

    Fix space image processing job

--- app/jobs/process_space_picture_job.rb
+++ app/jobs/process_space_picture_job.rb
@@ -2,9 +2,9 @@ class ProcessSpacePictureJob < ActiveJob::Base
   queue_as :default
 
   def perform(space_picture_id)
+    picture = SpacePicture.find(space_picture_id)
     s3_object = S3_BUCKET.object(picture.temp_image_s3_key)
 
-    picture = SpacePicture.find(space_picture_id)
     picture.remote_image_url = picture.temp_image_url
     picture.temp_image_url = nil
     picture.temp_image_s3_key = nil
