fa9e6bba70d0bb2aa299508aef41a97b0d0b256d
Simon Soriano <simon0191@gmail.com>
2016-11-30 20:34:58 -0500

    Fix image processinf job

--- app/jobs/process_space_picture_job.rb
+++ app/jobs/process_space_picture_job.rb
@@ -3,7 +3,7 @@ class ProcessSpacePictureJob < ActiveJob::Base
 
   def perform(space_picture_id)
     picture = SpacePicture.find(space_picture_id)
-    picture.remote_image_url = picture.temp_image_url
+    picture.remote_image_url = S3_BUCKET.object(picture.temp_image_s3_key).public_url
     picture.temp_image_url = nil
     picture.temp_image_s3_key = nil
     picture.save!
