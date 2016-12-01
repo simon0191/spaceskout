7128ec8d1c4eb1a8fa8a1fcc7dc19a104a2af324
Simon Soriano <simon0191@gmail.com>
2016-11-30 19:43:36 -0500

    Remove s3 file deletion logic
    - Now S3 automatically deletes tmp files with an expiration rule

--- app/jobs/process_space_picture_job.rb
+++ app/jobs/process_space_picture_job.rb
@@ -3,13 +3,9 @@ class ProcessSpacePictureJob < ActiveJob::Base
 
   def perform(space_picture_id)
     picture = SpacePicture.find(space_picture_id)
-    s3_object = S3_BUCKET.object(picture.temp_image_s3_key)
-
     picture.remote_image_url = picture.temp_image_url
     picture.temp_image_url = nil
     picture.temp_image_s3_key = nil
     picture.save!
-
-    s3_object.delete
   end
 end
