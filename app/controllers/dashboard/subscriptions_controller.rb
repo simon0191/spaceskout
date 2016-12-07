726d1768bda3113c0a8b45de663d98ba14ce9c77
Simon Soriano <simon0191@gmail.com>
2016-12-06 20:35:33 -0500

    Small adjustment to subscription notice

--- app/controllers/dashboard/subscriptions_controller.rb
+++ app/controllers/dashboard/subscriptions_controller.rb
@@ -12,6 +12,7 @@ class Dashboard::SubscriptionsController < Dashboard::BaseController
       stripe_token: params[:stripe_token]
     )
     if form.valid? && form.save!
+      flash[:notice] = 'Subscription created'
       render json: {status: 'success'}, status: :created
     else
       render json: {status: 'error', errors: form.errors}, status: :unprocessable_entity
